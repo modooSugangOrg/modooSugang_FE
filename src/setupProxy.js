7464829f46e362da34169afb2791414820e2a026
plooox <insukim97@gmail.com>
2022-04-28 16:12:03 +0900

    [:test_tube: TEST] Api base URL 경로 변경 + setUpProxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    "/home",
+    "/api",
     createProxyMiddleware({
       target: process.env.REACT_APP_TEST_URL,
       changeOrigin: true,
